ae4d2b1766b57ff02bc5e1474330ebead5914020
MizuguchiAkito <mizuguchi_a@tdc.co.jp>
2022-07-13 14:29:44 +0900

    [add]complated add todo task

--- todo-list-react/src/App.js
+++ todo-list-react/src/App.js
@@ -1,39 +1,40 @@
-import React,{ useState, useRef} from "react";
-import Task from "./Task";
-import {v4 as uuidv4 } from 'uuid'; //ユニークなid生成
+import "./App.css";
+import TodoList from "./TodoList";
+import { useState, useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
+// uuid...ランダムなid自動生成
 
-function App() {
+import React from "react";
 
-  const [todo, setTodos] = useState([{id:1, name:"Todo1",completed:false}]);
-  //State
+const App = () => {
+
+  const [todos, setTodos] = useState([]);
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
 
   const todoNameRef = useRef();
 
-  const handleAddTodo = ()=>{
-    //タスクを追加する
+  const handleAddTodo = (e) => {
     const name = todoNameRef.current.value;
-    setTodos((prevtodos)=>{
-      return[...prevtodos, {id:uuidv4(), name:name, completed:false}];
-      //... <- スプレッド構文
+    if (name === "") return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
     });
     todoNameRef.current.value = null;
   };
 
-  const toggleTodo = (id) =>{
-    const newTodos = [...todo];
-    const todo = newTodos.find((todo) => todo.id === id);
-    //find->todo.idを引数とひとつづつ照らし合わせる
-    todo.completed = !todo.completed;
-    setTodos(newTodos);
-  }
-
   return (
     <>
-      <Task todo={todo} toggleTodo={toggleTodo} />
-      <input type="text" ref={todoNameRef}></input>
-      <button onClick={handleAddTodo}>タスクを追加</button>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input type="text" name="" id="" ref={todoNameRef} />
+      <button onClick={handleAddTodo}>タスクの追加</button>
     </>
   );
-}
+};
 
 export default App;
