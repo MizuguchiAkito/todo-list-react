30fa91d3e4d525fae1bfdcb3961a5d7907b1bf37
MizuguchiAkito <mizuguchi_a@tdc.co.jp>
2022-07-14 16:02:41 +0900

    [add]edit todo task

--- todo-list-react/src/App.js
+++ todo-list-react/src/App.js
@@ -27,12 +27,13 @@ const App = () => {
     const name = todoNameRef.current.value;
     if (name === "") return;
     // タスクが空欄の時、タスクを追加しない処理
-
     setTodos((prevTodos) => {
       return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
     });
     todoNameRef.current.value = null;
   };
+
+
 
   const handleClear = () => {
     // タスクの削除処理
@@ -41,13 +42,37 @@ const App = () => {
     setTodos(newTodos);
   };
 
+
+  const handleEdit = (id) => {
+    // タスクの編集
+    const name = todoNameRef.current.value;
+    if (name === "") return;
+    // タスクが空欄の時、タスクを変更しない処理
+
+
+      const newTodos = [...todos];
+      const todo = newTodos.filter((todo) => todo.completed === true);
+    // filterで検索した値を全て格納
+      if(todo.length > 1){
+    // todoの値が1より大きい場合変更不可
+    // console.log("通ってる？")
+        return;
+      }else{
+    // console.log("成功なり");
+        const todo = newTodos.find((todo) => todo.completed === true);
+        todo.name = name;
+        setTodos(newTodos);
+      }
+  }
+
   return (
     <>
       <h3>Todoタスク管理</h3>
       <div>Remaining tasks : {todos.filter((todo) => !todo.completed).length}</div> {/* completedがTrueの時だけカウント */}
       <input type="text" name="" id="" ref={todoNameRef} />
       <button onClick={handleAddTodo}>Add</button>
-      <button onClick={handleClear}>Del</button><br/>
+      <button onClick={handleClear}>Del</button>
+      <button onClick={handleEdit}>Edit</button>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
     </>
   );
